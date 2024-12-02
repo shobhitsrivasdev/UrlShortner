84b48698159e1937a5a444a8d1d7cdbb10504f26
shobhitsrivasdev <shobhitcse0710@gmail.com>
2024-12-02 15:30:00 -0500

    Assets

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,7 +1,7 @@
-import {Button} from "@/components/ui/button";
-import {Input} from "@/components/ui/input";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Accordion,
   AccordionContent,
@@ -42,36 +42,6 @@ const LandingPage = () => {
         src="/banner1.jpg" // replace with 2 in small screens
         className="w-full my-11 md:px-11"
       />
-      <Accordion type="multiple" collapsible className="w-full md:px-11">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            How does the Trimrr URL shortener works?
-          </AccordionTrigger>
-          <AccordionContent>
-            When you enter a long URL, our system generates a shorter version of
-            that URL. This shortened URL redirects to the original long URL when
-            accessed.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>
-            Do I need an account to use the app?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes. Creating an account allows you to manage your URLs, view
-            analytics, and customize your short URLs.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>
-            What analytics are available for my shortened URLs?
-          </AccordionTrigger>
-          <AccordionContent>
-            You can view the number of clicks, geolocation data of the clicks
-            and device types (mobile/desktop) for each of your shortened URLs.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
     </div>
   );
 };
